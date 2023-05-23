84a516479551f275e5285378fb6a445bed255501
Ahmed Gamal <ahmedgamal.miner@gmail.com>
2023-05-23 08:22:43 +0300

    persist data by file manager system

---
+++ toDoTest/Item.swift
@@ -0,0 +1,12 @@
+//
+//  Item.swift
+//  toDoTest
+//
+//  Created by AHMED GAMAL  on 23/05/2023.
+//
+
+import Foundation
+struct Item : Codable{
+    var title = ""
+    var done = false
+}
--- toDoTest/ToDoViewController.swift
+++ toDoTest/ToDoViewController.swift
@@ -9,16 +9,17 @@ import UIKit
 
 class ToDoViewController: UITableViewController {
 
-    var itemArray : [String] = []
+    var itemArray : [Item] = []
     let defaults = UserDefaults.standard
+    
+    let fileDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appending(path: "items")
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        if let  dataArray = defaults.array(forKey: "itemArray") as? [String]{
-            itemArray = dataArray
-        }
+        loadData()
     }
     // tableview dataSource methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -26,18 +27,15 @@ class ToDoViewController: UITableViewController {
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell" , for: indexPath)
-            cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        cell.accessoryType = itemArray[indexPath.row].done == true ? .checkmark : .none
         return cell
     }
     
     //tableview delegate methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        saveData()
         tableView.deselectRow(at: indexPath, animated: true)
      }
     
@@ -47,7 +45,8 @@ class ToDoViewController: UITableViewController {
         let alert = UIAlertController(title: "add new Item", message: "", preferredStyle: .alert)
       
         let action = UIAlertAction(title: "Add", style: .default) { _ in
-            self.itemArray.append(addingTextField.text ?? "new Item")
+            let newItem = Item(title: addingTextField.text ?? "new Item")
+            self.itemArray.append(newItem)
             self.saveData()
         }
         
@@ -61,10 +60,29 @@ class ToDoViewController: UITableViewController {
     }
     
     func saveData(){
-        self.defaults.set(self.itemArray, forKey: "itemArray")
+        
+        
+        let encoder = JSONEncoder()
+        do {
+            let data =  try encoder.encode(self.itemArray)
+            try data.write(to: fileDirectory)
+        }catch{
+            print(error.localizedDescription)
+        }
         self.tableView.reloadData()
     }
     
-    
+    func loadData(){
+        let decoder = JSONDecoder()
+        do{
+            let data = try Data(contentsOf: fileDirectory)
+            itemArray = try decoder.decode([Item].self, from: data)
+        }catch {
+            print(error.localizedDescription)
+            
+        }
+       
+
+    }
 }
 
